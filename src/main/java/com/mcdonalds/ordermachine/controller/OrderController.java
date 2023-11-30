979358872ceb1bad2f349be861ddb607c171b029
AveePB <mike303.py@gmail.com>
2023-11-30 17:56:13 +0100

    Updated the order controller

--- src/main/java/com/mcdonalds/ordermachine/controller/OrderController.java
+++ src/main/java/com/mcdonalds/ordermachine/controller/OrderController.java
@@ -1,15 +1,24 @@
 package com.mcdonalds.ordermachine.controller;
 
 import com.mcdonalds.ordermachine.model.Product;
+import com.mcdonalds.ordermachine.model.ProductType;
 import com.mcdonalds.ordermachine.repository.OrderedProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -26,7 +35,7 @@ public class OrderController {
     }
 
 
-    @GetMapping("/{requestedId}")
+    @GetMapping("/id={requestedId}")
     private ResponseEntity<Product> findById(@PathVariable Long requestedId) {
         Optional<Product> productOptional = this.orderedProductRepository.findById(requestedId);
 
@@ -34,5 +43,29 @@ public class OrderController {
             return ResponseEntity.ok(productOptional.get());
 
         return ResponseEntity.notFound().build();
+    }
+
+
+    @GetMapping("/type={requestedType}")
+    private ResponseEntity<List<Product>> findAllByType(@PathVariable ProductType requestedType, Pageable pageable) {
+        Page<Product> page = this.orderedProductRepository.findByType(requestedType,
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        Sort.by("name")
+                ));
+        return ResponseEntity.ok(page.getContent());
+    }
+
+
+    @GetMapping("/receiptCode={requestedReceiptCode}")
+    private ResponseEntity<List<Product>> findAll(@PathVariable String requestedReceiptCode, Pageable pageable) {
+        Page<Product> page = this.orderedProductRepository.findByReceiptCode(requestedReceiptCode,
+                PageRequest.of(
+                    pageable.getPageNumber(),
+                    pageable.getPageSize(),
+                    Sort.Direction.ASC
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 }
--- src/main/java/com/mcdonalds/ordermachine/repository/OrderedProductRepository.java
+++ src/main/java/com/mcdonalds/ordermachine/repository/OrderedProductRepository.java
@@ -2,11 +2,17 @@ package com.mcdonalds.ordermachine.repository;
 
 import com.mcdonalds.ordermachine.model.Product;
 
+import com.mcdonalds.ordermachine.model.ProductType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface OrderedProductRepository extends CrudRepository<Product, Long> {
-
+public interface OrderedProductRepository extends CrudRepository<Product, Long>, PagingAndSortingRepository<Product, Long> {
+    //Spring Boot generated functions:
+    Page<Product> findByReceiptCode(String ReceiptCode, PageRequest pageRequest);
+    Page<Product> findByType(ProductType type, PageRequest pageRequest);
 }
