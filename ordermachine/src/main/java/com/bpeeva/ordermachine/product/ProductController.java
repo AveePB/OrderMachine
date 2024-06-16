e4ba6ccdacb15fdd6d4438dff322b96d702cb61a
AveePB <94467965+AveePB@users.noreply.github.com>
2024-06-16 16:37:59 +0200

    Update ProductController.java

--- ordermachine/src/main/java/com/bpeeva/ordermachine/product/ProductController.java
+++ ordermachine/src/main/java/com/bpeeva/ordermachine/product/ProductController.java
@@ -1,36 +1,45 @@
 package com.bpeeva.ordermachine.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/menu")
 @RequiredArgsConstructor
 public class ProductController {
 
-    @GetMapping
-    private ResponseEntity<String> getAllProducts() {
+    private final ProductService productService;
 
-        return null;
+    @GetMapping
+    private ResponseEntity<List<ProductDTO>> getAllProducts() {
+
+        return ResponseEntity.ok(productService.getAll());
     }
 
     @PostMapping
     private ResponseEntity<String> addProduct(@RequestBody ProductDTO request) {
+        boolean isAdded = productService.add(request.name(), request.price());
+        if (isAdded) return ResponseEntity.noContent().build();
 
-        return null;
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
     @PatchMapping
     private ResponseEntity<String> updateProductPrice(@RequestBody ProductDTO request) {
+        boolean isUpdated = productService.updatePrice(request.name(), request.price());
+        if (isUpdated) return ResponseEntity.noContent().build();
 
-        return null;
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{productName}")
     private ResponseEntity<String> removeProduct(@PathVariable String productName) {
-
-        return null;
+        productService.remove(productName);
+        return ResponseEntity.noContent().build();
     }
 }
